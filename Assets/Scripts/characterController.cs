64ec0a5217b18ed692efc31ac51e3f8f8b9ac42f
Даниил Кузнецов <den@webpp.ru>
2017-09-29 15:47:09 +0300

    метод выхода в паузу, доработка метода прыжка

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -3,10 +3,18 @@ using System.Collections;
 
 public class characterController : MonoBehaviour
 {
-	public float maxSpeed = 10;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+
+    public float attackSpeed = 1;
     public float jumpForce = 400;
+    public float maxSpeed = 10;
+    public int damage = 1;
+    public int heals = 1;
 
     private float speed;
+    private float groundRadius = 0.2;
+    private bool isPause = false;
     private bool isOnGround = false;
 
 	private void Start()
@@ -15,10 +23,15 @@ public class characterController : MonoBehaviour
     }
 
     /**
-     * Âûחûגאועסÿ םא ךאזהûי פנויל
+     * update every 1 frames
      */
     private void FixedUpdate()
     {
+        this.isOnGround = Physics2D.OverlapCircle(
+                this.groundCheck.position,
+                this.groundRadius,
+                this.whatIsGround
+            );//is work?
         this.speed = Input.GetAxisRaw("Horizontal");
     }
 
@@ -26,6 +39,7 @@ public class characterController : MonoBehaviour
     {
         HorizontalMove();
         Jump();
+        Pause();
     }
 
 	private void HorizontalMove()
@@ -35,9 +49,41 @@ public class characterController : MonoBehaviour
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-        {
+        if (this.isOnGround && isJumpKeyDown()) {
             GetComponent<Rigidbody2D>().AddForce(new Vector2(1, this.jumpForce));
         }
+    }
+
+    private void Pause()
+    {
+        if (IsPauseKeyDown()) {
+            if (!this.isPause) {
+                PauseOn();
+            } else {
+                PauseOff();
+            }
+        }
+    }
+
+    private void PauseOn()
+    {
+        Time.timeScale = 0;
+        this.isPause = true;
+    }
+
+    private void PauseOff()
+    {
+        Time.timeScale = 1;
+        this.isPause = false;
+    }
+
+    private bool IsPauseKeyDown()
+    {
+        return Input.GetKeyDown(KeyCode.ESC);
+    }
+
+    private bool IsJumpKeyDown()
+    {
+        return (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow));
     }
 }
