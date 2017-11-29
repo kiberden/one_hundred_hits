3cb14cab4fa2b343eeccb538ebd7bb3f29b8e30b
Неушто-ПК\Неушто <pavlov102@gmail.com>
2017-11-29 21:08:26 +0300

    1. анимации атаки прыжка, спец.атаки, получение урона ГГ 2. Анимации кхорнита 3. Попотка заскриптовать кхорнита 4. Поправлены платформы - в один уровень выравнены

---
+++ Assets/Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Enemy : MonoBehaviour 
+{
+	public LayerMask enemyMask;
+	public float speed;
+	Rigidbody2D myBody;
+	Transform myTrans;
+	float myWidth;
+
+	void Start ()
+	{
+		myTrans = this.transform;
+		myBody = this.GetComponent<Rigidbody2D>();
+		myWidth = this.GetComponent<SpriteRenderer>().bounds.extents.x;
+
+	}
+
+	void FixedUpdate ()
+	{
+		Vector2 lineCastPos = myTrans.position - myTrans.right * myWidth;
+		Debug.DrawLine(lineCastPos, lineCastPos + Vector2.down);
+		bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down, enemyMask);
+
+		if(!isGrounded)
+		{
+			Vector3 currRot = myTrans.eulerAngles;
+			currRot.y += 180;
+			myTrans.eulerAngles = currRot;
+		}
+
+		Vector2 myVel = myBody.velocity;
+		myVel.x = -myTrans.right.x * speed;
+		myBody.velocity = myVel;
+	}
+}
