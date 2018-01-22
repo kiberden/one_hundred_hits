56a75d612170936d2874db8ec52bfd03bc66f30c
Helanir <seepedwella@gmail.com>
2018-01-23 00:29:30 +0300

    Скрипт звуков Звуки Падающая платформа Скрипт падающией платформы

--- Assets/Assets/Scripts/CharacterController.cs
+++ Assets/Assets/Scripts/CharacterController.cs
@@ -3,151 +3,154 @@ using System.Collections;
 
 public class CharacterController : MonoBehaviour
 {
-    public Transform groundCheck;
-    public LayerMask whatIsGround;
-    
-    public float jumpForce = 650;
-    public float maxSpeed = 10;
-    public int damage = 1;
-    public int heals = 5;
+	public Transform groundCheck;
+	public LayerMask whatIsGround;
 
-    private float speed;
-    private float groundRadius = 0.2f;
-    private bool isPause = false;
-    private bool isOnGround = false;
-    private bool isShowRight = true;
-    
-    private BoxCollider2D weapon;
-    private Animator anim; //Анимация
+	public float jumpForce = 650;
+	public float maxSpeed = 10;
+	public int damage = 1;
+	public int heals = 5;
 
-    public string status = "";
+	private float speed;
+	private float groundRadius = 0.2f;
+	private bool isPause = false;
+	private bool isOnGround = false;
+	private bool isShowRight = true;
 
-    /*debug*/
-    public bool isAttack;
+	private BoxCollider2D weapon;
+	private Animator anim; //Анимация
 
-    private void Start()
+	public string status = "";
+
+	/*debug*/
+	public bool isAttack;
+
+	private void Start()
 	{
 		this.anim = GetComponent<Animator>();// для анимации, но это как обычно
-        this.weapon = GameObject.Find("/hero/Sword/").GetComponent<BoxCollider2D>();
-    }
+		this.weapon = GameObject.Find("/hero/Sword/").GetComponent<BoxCollider2D>();
+	}
 
-    /**
+	/**
      * update every 1 frames
      */
-    private void FixedUpdate()
-    {
-        this.isOnGround = Physics2D.OverlapCircle(
-                this.groundCheck.position,
-                this.groundRadius,
-                this.whatIsGround
-            );
-        this.speed = Input.GetAxisRaw("Horizontal");
-    }
+	private void FixedUpdate()
+	{
+		this.isOnGround = Physics2D.OverlapCircle(
+			this.groundCheck.position,
+			this.groundRadius,
+			this.whatIsGround
+		);
+		this.speed = Input.GetAxisRaw("Horizontal");
+	}
 
-    private void Update()
-    {
-        this.anim.SetFloat("Speed", Mathf.Abs(this.speed));
+	private void Update()
+	{
+		this.anim.SetFloat("Speed", Mathf.Abs(this.speed));
 
-        HorizontalMove();
-        Attack();
-        Jump();
-        Pause();
+		HorizontalMove();
+		Attack();
+		Jump();
+		Pause();
 
-        FlipHero();
-    }
+		FlipHero();
+	}
 
-    private void FlipHero()
-    {
-        if (this.speed > 0 && !this.isShowRight) {
-            Flip(true);
-        } else if (this.speed < 0 && this.isShowRight) {
-            Flip(false);
-        }
-    }
+	private void FlipHero()
+	{
+		if (this.speed > 0 && !this.isShowRight) {
+			Flip(true);
+		} else if (this.speed < 0 && this.isShowRight) {
+			Flip(false);
+		}
+	}
 
-    private void Attack()
-    {
-        if (Input.GetKeyDown(KeyCode.E) && !this.anim.GetBool("Attack")) {
-            /*debug*/
-            //var animatorStateInfo = this.anim.GetCurrentAnimatorStateInfo(0);
-            //animatorStateInfo.IsName("Player_attack");
-            /**/
+	private void Attack()
+	{
+		if (Input.GetKeyDown(KeyCode.E) && !this.anim.GetBool("Attack")) {
+			//SoundManager.PlaySound ("playerAttack");
+			/*debug*/
+			//var animatorStateInfo = this.anim.GetCurrentAnimatorStateInfo(0);
+			//animatorStateInfo.IsName("Player_attack");
+			/**/
 
-            this.anim.SetTrigger("Attack");
-            //this.StartAttackTrigger();
-            HeroMeleAttack.Action(this.weapon, "Enemy", this.damage, false);
-        }   
-    }
+			this.anim.SetTrigger("Attack");
+			//this.StartAttackTrigger();
+			HeroMeleAttack.Action(this.weapon, "Enemy", this.damage, false);
+		}   
+	}
 
-    private void Flip(bool flipOnRight)
-    {
-        this.isShowRight = flipOnRight;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    }
+	private void Flip(bool flipOnRight)
+	{
+		this.isShowRight = flipOnRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
 
 	private void HorizontalMove()
 	{
-        GetComponent<Rigidbody2D>().velocity = new Vector2(this.speed * this.maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
-    }
+		GetComponent<Rigidbody2D>().velocity = new Vector2(this.speed * this.maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+	}
 
-    private void Jump()
-    {
-        if (this.isOnGround && IsJumpKeyDown()) {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(1, this.jumpForce));
+	private void Jump()
+	{
+		if (this.isOnGround && IsJumpKeyDown()) {
+			//SoundManager.PlaySound ("playerJump");
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(1, this.jumpForce));
+
 		}
-    }
+	}
 
-    private void Pause()
-    {
-        if (IsPauseKeyDown()) {
-            if (!this.isPause) {
-                PauseOn();
-            } else {
-                PauseOff();
-            }
-        }
-    }
+	private void Pause()
+	{
+		if (IsPauseKeyDown()) {
+			if (!this.isPause) {
+				PauseOn();
+			} else {
+				PauseOff();
+			}
+		}
+	}
 
-    private void PauseOn()
-    {
-        Time.timeScale = 0;
-        this.isPause = true;
-    }
+	private void PauseOn()
+	{
+		Time.timeScale = 0;
+		this.isPause = true;
+	}
 
-    private void PauseOff()
-    {
-        Time.timeScale = 1;
-        this.isPause = false;
-    }
+	private void PauseOff()
+	{
+		Time.timeScale = 1;
+		this.isPause = false;
+	}
 
-    private bool IsPauseKeyDown()
-    {
-        return Input.GetKeyDown(KeyCode.Escape);
-    }
+	private bool IsPauseKeyDown()
+	{
+		return Input.GetKeyDown(KeyCode.Escape);
+	}
 
-    private bool IsJumpKeyDown()
-    {
-        return (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow));
-    }
+	private bool IsJumpKeyDown()
+	{
+		return (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow));
+	}
 
-    private void StartAttackTrigger()
-    {
-        this.weapon.enabled = true;
-    }
+	private void StartAttackTrigger()
+	{
+		this.weapon.enabled = true;
+	}
 
-    private void CompleteAttackTrigger()
-    {
-        this.weapon.enabled = false;
-        this.anim.ResetTrigger("Attack");
-        Debug.Log("end");
-    }
+	private void CompleteAttackTrigger()
+	{
+		this.weapon.enabled = false;
+		this.anim.ResetTrigger("Attack");
+		Debug.Log("end");
+	}
 
-    public bool IsAttackStatus()
-    {
-        return this.anim.GetBool("Attack");
-    }
+	public bool IsAttackStatus()
+	{
+		return this.anim.GetBool("Attack");
+	}
 
 
 
---
+++ Assets/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public static AudioClip playerHitSound, playerAttackSound, playerHealSound, playerJumpSound, playerPickupSound;
+	static AudioSource audioSrc;
+
+	// Use this for initialization
+	void Start () {
+
+		playerHitSound = Resources.Load<AudioClip> ("playerHit");
+		playerAttackSound = Resources.Load<AudioClip> ("playerAttack");
+		playerHealSound = Resources.Load<AudioClip> ("playerHeal");
+		playerJumpSound = Resources.Load<AudioClip> ("playerJump");
+		playerPickupSound = Resources.Load<AudioClip> ("playerPickup");
+
+		audioSrc = GetComponent<AudioSource> ();
+	
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static void PlaySound (string clip)
+	{
+		switch (clip) {
+		case "playerHit":
+			audioSrc.PlayOneShot (playerHitSound);
+			break;
+
+		case "playerAttack":
+			audioSrc.PlayOneShot (playerAttackSound);
+			break;
+
+		case "playerHeal":
+			audioSrc.PlayOneShot (playerHealSound);
+			break;
+
+		case "playerJump":
+			audioSrc.PlayOneShot (playerJumpSound);
+			break;
+
+		case "playerPickup":
+			audioSrc.PlayOneShot (playerPickupSound);
+			break;
+
+		}
+	}
+}
